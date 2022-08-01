6532eff20f2cd9c7f5487f18debf17edf488ac0d
sachinkumar07 <sachin@avidsecure.io>
2022-08-01 14:43:00 +0530

    PCG-11192 Added HTTP trigger function

--- activityLogsApp/HttpTriggerFunction.cs
+++ activityLogsApp/HttpTriggerFunction.cs
@@ -2,8 +2,7 @@ using System.IO;
 using System.Collections.Generic;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;\
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
@@ -12,7 +11,7 @@ namespace NwNsgProject
     public static class HttpTriggerFunction
     {
 		[FunctionName("HttpTriggerFunction")]
-		public static async IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
+		public static async Task Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req, ILogger log)
 		{
     		log.LogInformation("HTTP trigger function processed a request.");
 		}
